e8ac91f3a5668fd367abfe681cbebcd3cb8f362a
Javier Galindo <javigalindo.sopra@gmail.com>
2018-04-24 17:51:39 +0200

    Added internationalization to App

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 import thunk from "redux-thunk";
+import { loadTranslations, setLocale, syncTranslationWithStore } from "react-redux-i18n";
 
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import reducers from "./reducers";
+import translationsObject from "./assets/i18n";
 
 const store = createStore(reducers, applyMiddleware(promiseMiddleware, thunk));
+
+syncTranslationWithStore(store);
+store.dispatch(loadTranslations(translationsObject));
+store.dispatch(setLocale("es"));
 
 ReactDOM.render(
   <Provider store={store}>
