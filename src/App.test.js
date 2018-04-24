b2fec06543917087410227bb7f0227a1d302221c
Javier Galindo <javigalindo.sopra@gmail.com>
2018-04-24 10:01:06 +0200

    Added tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,41 @@
-/* global it, describe, expect,jest  */
+/* global it, describe, expect, beforeEach */
 
 import React from "react";
 import { mount } from "enzyme";
-import { App } from "./App";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import configureStore from "redux-mock-store";
 
-function setup() {
-  const props = {
-    dispatch: jest.fn(),
-    contributors: { unassigned: [{ first_name: "PEPE", id: 2 }] },
-  };
-  const enzymeWrapper = mount(<App {...props} />);
-
-  return {
-    enzymeWrapper,
-  };
-}
+import App from "./App";
 
 describe("Table contributors without workplace", () => {
-  it("should render self and subcomponents", () => {
-    const { enzymeWrapper } = setup();
+  let enzymeWrapper;
 
+  beforeEach(() => {
+    const initialState = {
+      contributors: {
+        unassigned: [{ first_name: "PEPE", id: 2, workroom: 1 }, { first_name: "ALEX", id: 3 }],
+      },
+    };
+
+    const mock = new MockAdapter(axios);
+    mock.onGet("/contributors/unassigned.json").reply(200, {});
+
+    const mockStore = configureStore();
+    const store = mockStore(initialState);
+    enzymeWrapper = mount(<App store={store} />);
+  });
+
+  it("should render self and subcomponents", () => {
     expect(enzymeWrapper.find("Grid").prop("fluid")).toBe(true);
 
     expect(enzymeWrapper.find("Row").hasClass("show-grid")).toBe(true);
     expect(enzymeWrapper.find("Table").exists()).toBe(true);
+  });
 
-    /* const todoInputProps = enzymeWrapper.find("TodoTextInput").props();
-    expect(todoInputProps.newTodo).toBe(true);
-    expect(todoInputProps.placeholder).toEqual("What needs to be done?"); */
+  it("Should check if contributor has workroom", () => {
+    expect(enzymeWrapper.find(".unassigned").length).toEqual(1);
+    expect(enzymeWrapper.find(".overocupation").length).toEqual(1);
   });
 
   // it('renders only contributors without workplace', () =>{
